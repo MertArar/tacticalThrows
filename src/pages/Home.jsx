1b0afe49eb5e72d50f3f02276f4f32859a18c3ee
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-29 04:01:24 +0300

    Home page developed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import FeaturedCard from "../components/FeaturedCard";
+import HomeHeader from "../components/HomeHeader";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return <div>Home</div>;
