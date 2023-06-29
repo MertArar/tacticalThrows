1e307a7d7e4ddf170e9b5839cecee23e20621686
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-29 03:52:50 +0300

    tailwind tested

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <div className="text-white">test</div>
+    </div>
+  );
 }
 
 export default App;
