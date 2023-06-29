b1dc47387b6a7c2c17c8ac987a00676dd610b860
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-29 04:04:49 +0300

    Home added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Featured from "../data/featured.json";
 import FeaturedCard from "../components/FeaturedCard";
 import HomeHeader from "../components/HomeHeader";
 import Footer from "../components/Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  console.log(Featured);
+
+  return (
+    <div>
+      <header>
+        <Navbar />
+        <HomeHeader />
+      </header>
+
+      <main>
+        <FeaturedCard />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
