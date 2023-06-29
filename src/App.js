620ded12653f286567269e0a35e1aa727674588a
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-29 04:00:17 +0300

    Main app file added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import MapMirage from "./pages/MapMirage";
+import MapDust2 from "./pages/MapDust2";
+import MapInferno from "./pages/MapInferno";
+import Grenades from "./pages/Grenades";
+import Blogs from "./pages/Blogs";
+import Test from "./pages/Test";
+import SubmitLineup from "./pages/SubmitLineup";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="text-white">test</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/test" element={<Test />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/submit" element={<SubmitLineup />} />
+          <Route path="/mirage" element={<MapMirage />} />
+          <Route path="/dust2" element={<MapDust2 />} />
+          <Route path="/inferno" element={<MapInferno />} />
+
+          <Route path="/grenades" element={<Grenades />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
